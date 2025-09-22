81fb41feab6a340303811ca66b0b3170986d5117
Lionel <minato.nami.dev@gmail.com>
2025-09-22 11:52:00 +0800

    Complete chapter 6 CRUD forms

--- ch5-blog-app/blog/views.py
+++ ch5-blog-app/blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-# Create your views here.
 
 
 class BlogListView(ListView):
@@ -12,3 +12,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'body', 'author']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
