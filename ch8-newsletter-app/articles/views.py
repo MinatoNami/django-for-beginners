e5bcf2f28bed2cc355ece24f6f3bbe2150df91eb
Lionel <minato.nami.dev@gmail.com>
2025-09-23 14:43:59 +0800

    Add mixins for permissions

--- ch8-newsletter-app/articles/views.py
+++ ch8-newsletter-app/articles/views.py
@@ -1,33 +1,46 @@
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Article
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ['title', 'body']
     template_name = "article_edit.html"
     success_url = reverse_lazy('article_list')
 
+    def test_func(self):
+        article = self.get_object()
+        return self.request.user == article.author
 
-class ArticleDeleteView(DeleteView):
+
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy('article_list')
 
+    def test_func(self):
+        article = self.get_object()
+        return self.request.user == article.author
 
-class ArticleCreateView(CreateView):
+
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
-    fields = ['title', 'body', 'author']
+    fields = ['title', 'body']
     template_name = "article_new.html"
     success_url = reverse_lazy('article_list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
